645cbff6a162df464e3ec8e560051fc7b7232c88
Francesco <fra.fatini1@gmail.com>
2023-02-02 16:16:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 console.log("siamo on, baby");
+
+let express = require("express");
+let app = express();
+let port = 3000;
+
+let server = app.listen(port);
+console.log("running server on http://localhost:" + port);
